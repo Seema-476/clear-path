144b3c9a5464c0190072429ff2781c0e274f53d4
Seema-476 <simmybishnoi29085@gmail.com>
2024-10-04 16:29:29 +0530

    clear path page

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// scroll top btn 
+const topBtn = document.querySelector(".topBtn")
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 60) {
+        topBtn.classList.add("show")
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
+const toggler = document.getElementById("toggler");
+toggler.addEventListener("click", function () {
+    toggler.classList.toggle("align-items-center");
+});
